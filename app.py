3e8bbd732c166defdb47e609212d596a02083129
kesa0v0 <kesa0v0@users.noreply.github.com>
2020-08-31 22:16:03 +0900

    d

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ app = flask.Flask(__name__)
 
 storage = "./storage/"
 
+
 @app.route('/')
 def index():
     return flask.render_template('index.html')
+
 
 @app.route('/upload', methods=['GET', 'POST'])
 def uploading():
@@ -20,14 +22,14 @@ def uploading():
         if not files:
             return "선택되지 않음"
         else:
-            code = padding(6, str(hex(random.randint(0, 16**6-1)))[2:]).upper()
+            code = padding(6, str(hex(random.randint(0, 16 ** 6 - 1)))[2:]).upper()
             if not os.isdir(storage):
                 os.mkdir(storage)
             os.mkdir(storage + code)
 
             for file in files:
                 file.save(storage + code + '/' + file.filename)
-            
+
             ziped = zipfile.ZipFile(storage + code + '.zip', 'w')
             zipdir(storage + code, ziped)
             ziped.close()
@@ -35,6 +37,7 @@ def uploading():
             rmtree(storage + code)
 
             return code
+
 
 @app.route('/download', methods=['GET', 'POST'])
 def downloading():
@@ -45,19 +48,22 @@ def downloading():
         file = storage + code + '.zip'
         print(file)
 
-        return flask.send_from_directory(storage, code+'.zip', as_attachment=True)
+        return flask.send_from_directory(storage, code + '.zip', as_attachment=True)
+
 
 def padding(size, value):
     diff = size - len(str(value))
     if diff > 0:
         value = diff * "0" + value
-    
+
     return value
+
 
 def zipdir(path, ziph):
     for root, dirs, files in os.walk(path):
         for file in files:
             ziph.write(os.path.join(root, file), compress_type=zipfile.ZIP_DEFLATED)
+
 
 if __name__ == "__main__":
     app.run()
